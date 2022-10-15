17221e53eeb8e147bd9f2c5d75568d3640d60675
Костя-ПК\Костя <kostya.valkovski.work@gmail.com>
2022-10-15 19:04:18 +0300

    Модуль 4 Юнит 4,1

---
+++ Module4.1/Module4.1.1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Module4._1._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "" ; 
+            string b ="" ;
+            bool c = a != b;
+
+            Console.WriteLine(c);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Module4.1/Module4.1.5/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Module4._1._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 10;
+            double x = 2.3;
+            double y = 4.7;
+            bool c = (a < b) | (x > y);
+
+            Console.WriteLine(c);
+
+            Console.ReadKey();
+        }
+    }
+}
