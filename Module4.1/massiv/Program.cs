01dd2e9d93b01a1c534bcd3c4d725dc088c25fe8
Kostya <113539367+KonstantinValkovsky@users.noreply.github.com>
2023-04-15 12:27:58 +0300

    работа с массивом

--- Module4.1/massiv/Program.cs
+++ Module4.1/massiv/Program.cs
@@ -4,6 +4,8 @@
     {
         static void Main(string[] args)
         {
+            int[,] array = { { 1, 2, 3 }, { 5, 6, 7 } };
+
             Console.WriteLine("Введите своё имя");
 
             var name = Console.ReadLine();
@@ -16,6 +18,14 @@
             }
 
             Console.WriteLine("Последняя буква вашего имени: {0}", name[name.Length - 1]);
+
+            Console.WriteLine(array.Length);
+
+            Console.Write("Количество строк: ");
+            Console.WriteLine(array.GetUpperBound(0) + 1 + " ");
+
+            Console.Write("Количество колонок: ");
+            Console.WriteLine(array.GetUpperBound(1) + 1 + " ");
 
             Console.ReadKey();
             
